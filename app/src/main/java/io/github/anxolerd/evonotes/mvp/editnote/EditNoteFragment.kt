271725b5f276012c824b8bea7a78f4a6960b90b3
Oleksandr Kovalchuk <anxolerd@outlook.com>
2019-06-09 16:42:19 +0300

    Add notes sharing

--- app/src/main/java/io/github/anxolerd/evonotes/mvp/editnote/EditNoteFragment.kt
+++ app/src/main/java/io/github/anxolerd/evonotes/mvp/editnote/EditNoteFragment.kt
@@ -1,10 +1,16 @@
 package io.github.anxolerd.evonotes.mvp.editnote
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.*
 import android.widget.EditText
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import io.github.anxolerd.evonotes.R
 import io.github.anxolerd.evonotes.dto.Note
+
 
 private const val NOTE_ID = "note_id"
 private const val NEW_NOTE_ID = -1L
@@ -21,6 +27,8 @@ class EditNoteFragment : androidx.fragment.app.Fragment(), EditNoteView {
     }
 
     private var noteId: Long? = null
+    private var shareActionProvider: ShareActionProvider? = null
+    private var shareIntent: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +48,24 @@ class EditNoteFragment : androidx.fragment.app.Fragment(), EditNoteView {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_edit_note, container, false)
         noteEditText = view.findViewById(R.id.note_edit_text)
+        noteEditText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                setShareIntent(s.toString())
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+            }
+        })
 
         return view
     }
@@ -47,6 +73,16 @@ class EditNoteFragment : androidx.fragment.app.Fragment(), EditNoteView {
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.menu_edit, menu)
+        val shareItem = menu?.findItem(R.id.action_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(shareItem) as ShareActionProvider?
+        shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent!!.type = "text/plain"
+        setShareIntent(this.noteEditText?.text.toString())
+    }
+
+    private fun setShareIntent(text: String) {
+        this@EditNoteFragment.shareIntent?.putExtra(Intent.EXTRA_TEXT, text)
+        this@EditNoteFragment.shareActionProvider?.setShareIntent(shareIntent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
